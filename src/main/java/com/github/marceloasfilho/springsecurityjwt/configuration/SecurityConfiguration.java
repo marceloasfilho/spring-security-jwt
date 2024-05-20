13988d7e3e187ea29b4ab9e5a3f3908cd82166ac
marcelo.filho <marceloandradesilvafilho@gmail.com>
2024-05-20 13:21:17 -0300

    - feat: adicionando bean AuthenticationManager

--- src/main/java/com/github/marceloasfilho/springsecurityjwt/configuration/SecurityConfiguration.java
+++ src/main/java/com/github/marceloasfilho/springsecurityjwt/configuration/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.github.marceloasfilho.springsecurityjwt.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -46,6 +48,11 @@ public class SecurityConfiguration {
         authenticationProvider.setUserDetailsService(this.userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
